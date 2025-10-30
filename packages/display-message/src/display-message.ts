3e18ec21148dc04df923a12f0d7b578fb34117a1
christian-brindley <christian.brindley@forgerock.com>
2025-10-30 10:31:06 +0000

    initial version

--- packages/display-message/src/display-message.ts
+++
@@ -1,9 +0,0 @@
-import { execNode } from "@imports/common/node-utils";
-
-var nodeOutcomes = {
-  COMPLETED: "completed",
-};
-
-execNode(() => {
-  // Node logic goes here
-}, nodeOutcomes.COMPLETED);
