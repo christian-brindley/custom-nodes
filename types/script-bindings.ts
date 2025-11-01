42fe82f2c46b03ee727f1d057b636c8a264aa84b
christian-brindley <christian.brindley@forgerock.com>
2025-11-01 18:43:24 +0000

    bindings

--- types/script-bindings.ts
+++ types/script-bindings.ts
@@ -9,15 +9,28 @@ declare const action: {
 
 declare const callbacksBuilder: {
   textOutputCallback: (level: number, message: string) => void;
+  stringAttributeInputCallback: (
+    id: string,
+    label: string,
+    value: string,
+    required: boolean,
+    failedPolicies: string[]
+  ) => void;
 };
 
 declare const properties: any;
 
 declare const nodeState: {
-  remove: (property: string) => null;
+  remove: (property: string) => void;
   get: (property: string) => any;
+  getObject: (property: string) => any;
+  putShared: (property: string, value: any) => void;
+  mergeShared: (property: string, value: any) => void;
+  mergeTransient: (property: string, value: any) => void;
+  putTransient: (property: string, value: any) => void;
 };
 
 declare const callbacks: {
   isEmpty: () => boolean;
+  getStringAttributeInputCallbacks: () => any;
 };
